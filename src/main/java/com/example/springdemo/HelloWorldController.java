f191ccf72b9fa80b762b0c6c20c4ab05f39c4002
nurhatmurathan <nurhatmurathan001@gmail.com>
2023-06-27 12:25:17 +0600

    initial spring progect hello world

---
+++ src/main/java/com/example/springdemo/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.example.springdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, Spring!";
+    }
+
+}
